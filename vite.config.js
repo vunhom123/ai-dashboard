13d04a355ca53345a2b66f9772bc695dbcb1ce74
Vunhom123 <nnv242@gmail.com>
2026-04-01 23:27:23 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-});
+export default {
+  build: {
+    chunkSizeWarningLimit: 500,
+  },
+};
